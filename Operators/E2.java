6192d4d7c795ebbf2b655a2bfa786adc2705d2a1
AlejandrosMiralles <alumir3453@ieselcaminas.org>
2022-01-21 17:37:50 +0100

    Operadores: Ejercicios 1-4

---
+++ Operators/E2.java
@@ -0,0 +1,17 @@
+class E2{
+
+    float value;
+
+    static void reValueF(E2 object){ object.value = 345;}
+    public static void main(String[] args) {
+        E2 tester1 = new E2();
+
+        tester1.value = 23;
+
+        System.out.println("Float = "+ tester1.value);
+
+        reValueF(tester1);
+
+        System.out.println("Float = "+tester1.value);
+    }
+}
---
+++ Operators/E4.java
@@ -0,0 +1,10 @@
+public class E4 {
+    public static void main(String[] args) {
+        final float D = 40, T = 5; //Distancia y tiempo
+        float v; //Velocidad
+
+        v = D / T;
+
+        System.out.println(v);
+    }
+}
