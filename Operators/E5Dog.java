66da705b0f007268d84de71f290230918fef7759
AlejandrosMiralles <alumir3453@ieselcaminas.org>
2022-01-24 17:11:07 +0100

    Operadores: Ejercicios 5-7

---
+++ Operators/E5Dog.java
@@ -0,0 +1,21 @@
+public class E5Dog {
+    String name;
+    String says;
+
+    E5Dog(String dogName, String dogSays){
+        name = dogName;
+        says = dogSays;
+    }
+
+    public static void main(String[] args) {
+        E5Dog dog1 = new E5Dog("Spot", "Ruff");
+        E5Dog dog2 = new E5Dog("Scruffy", "Wurf");
+
+        System.out.printf("Dog1:\n\t\tName: %s\n\tSays: %s\n\nDog2:\n\t\tName: %s\n\tSays: %s", dog1.name, dog1.says,
+                                dog2.name, dog2.says);
+
+        E5Dog dogTest = dog1;
+
+        System.out.println("\n\nDog1.equals(dogTest): "+dog1.equals(dogTest)+"\nDog1 == DogTest: "+(dog1==dogTest));
+    }
+}
---
+++ Operators/E7.java
@@ -0,0 +1,14 @@
+import java.util.Date;
+
+public class E7 {
+    public static void main(String[] args) {
+        Date object = new Date();
+
+        System.out.println(object.hashCode());
+        if (object.hashCode()%2==0){
+            System.out.println("Cara");
+        }else{
+            System.out.println("Cruz");
+        }
+    }
+}
