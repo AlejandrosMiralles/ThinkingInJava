593de47fe190b5a75e8e3d9305c2bd24e57cdb0f
AlejandrosMiralles <alumir3453@ieselcaminas.org>
2022-05-06 00:14:30 +0200

    sfdv

---
+++ String/Ex11RegularExpression.java
@@ -0,0 +1,15 @@
+package String;
+
+import java.util.regex.Pattern;
+
+public class Ex11RegularExpression {
+    public static void main(String[] args) {
+        String frase ="Airline ate eight apples and one orange ile anita adnt any";
+        String patron = "(?i)(((^[aeiou])|(\s+[aeiou]))\\w+?\b)";
+
+        for (String string : frase.split(patron)) {
+            System.out.println(string);
+        }
+    }
+
+}
