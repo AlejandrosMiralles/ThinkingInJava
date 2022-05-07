3e5851d35a46cf6281ed40719face631900264cf
AlejandrosMiralles <alumir3453@ieselcaminas.org>
2022-05-07 18:16:00 +0200

    Ejercicios 8-9 RTTI hechos

--- TypeInformation/Ex8And9DiscoveringHierarchyAtRT.java
+++ TypeInformation/Ex8And9DiscoveringHierarchyAtRT.java
@@ -1,6 +1,55 @@
 package TypeInformation;
 
+
 public class Ex8And9DiscoveringHierarchyAtRT {
-    private static class Ex8{}
-    private static class Ex9{}
+    /*
+    private static class Ex8{
+        static void printingTree(Object youngestCLass){
+            if (youngestCLass == null) return;
+            ArrayList<Class<Object>> classes = new ArrayList<Class<Object>>();
+            
+            for (Class actualClass = youngestCLass.getClass(); actualClass != null; 
+                        actualClass = actualClass.getSuperclass()) {
+                classes.add(actualClass);
+            }
+
+            for (Class class1 : classes) {
+                System.out.println(class1.getName() + "\n");
+            }
+            
+        }
+    }
+    */
+    /*
+    private static class Ex9{
+        static void printingTree(Object youngestCLass){
+            if (youngestCLass == null) return;
+
+            ArrayList<Class<Object>> classes = new ArrayList<Class<Object>>();
+            
+            for (Class actualClass = youngestCLass.getClass(); actualClass != null; 
+                        actualClass = actualClass.getSuperclass()) {
+                classes.add(actualClass);
+            }
+
+            for (Class class1 : classes) {
+                System.out.println(class1.getSimpleName() + ":\t");
+                try {
+                    for (Field field : class1.getDeclaredFields()) {
+                        System.out.println(field.getType() + ":" + field.getName() + "\n");
+                    }
+                } catch (Exception e) {
+                    System.out.println(e);
+                }
+                System.out.println("\n");
+            }
+            
+        }
+    }
+    */
+    public static void main(String[] args) {
+        //Ex8.printingTree(null);
+
+        //Ex9.printingTree(null);
+    }
 }
